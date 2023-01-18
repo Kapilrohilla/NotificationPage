2a6832603f56709f5283e182c8335cfd1beea282
kapilRohilla <kapilrohilla2002@gmail.com>
2023-01-18 14:19:05 +0530

    final design ready

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('hello');
+const mrkRead = document.getElementsByTagName('button')[0];
+const notification = document.getElementsByClassName('notifi');
+
+console.log(notification);
+mrkRead.addEventListener('button',()=>{
+})
