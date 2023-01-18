721a0647708b0535b91ef9ffb52c762af1234371
kapilRohilla <kapilrohilla2002@gmail.com>
2023-01-18 18:03:38 +0530

    this is final.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const mrkRead = document.getElementsByTagName('button')[0];
+let count = document.getElementById('count');
 const notification = document.getElementsByClassName('notifi');
+console.log(notification[1].classList.add('unread'));
+// present count
+let notiCount = count.innerText;
 
-console.log(notification);
-mrkRead.addEventListener('button',()=>{
-})
+
+function togBtn (){
+}
