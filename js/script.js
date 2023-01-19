714fb29c2b492e83f08148717db035b7652286dc
kapilRohilla <kapilrohilla2002@gmail.com>
2023-01-19 18:07:51 +0530

    Red Mark added

--- js/script.js
+++ js/script.js
@@ -3,22 +3,29 @@ let docCount = document.getElementById('count')
 let notification = document.querySelectorAll('.notifi');
 
 notification.forEach((a)=>{
+    // let time = a.children[a.children.length - 1].contains('time');
+    // console.log(time)
     count = parseInt(docCount.innerHTML);
         a.addEventListener('click',()=>{
             if(a.classList.contains('unread')){
                 a.classList.remove('unread');
-                console.log('class femoved');
+                // console.log('class femoved');
                 count -= 1;
                 if(count <= 0){
                     count=0;
                 }
                 docCount.innerHTML = count;
+                a.children[a.children.length - 1].remove();
                 
             }else{
                 a.classList.add('unread');
-                console.log('class added');
+                // console.log('class added');
                 count += 1;
-                docCount.innerHTML = count
+                docCount.innerHTML = count;
+                let newElement = document.createElement('span');
+                newElement.classList.add('redMark');
+                a.appendChild(newElement);
+                
             }
         })
 });
@@ -29,10 +36,4 @@ mrkRead.addEventListener('click',()=>{
         docCount.innerHTML = 0;
     })
 })
-
-drkMode
-const btn = document.getElementById('drkToggle').children[0];
-btn.addEventListener('toggle',()=>{
-    btn.children[0].style.display= "none";
-    btn.children[1].style.display= "inline-block";
-})
+// red mark
